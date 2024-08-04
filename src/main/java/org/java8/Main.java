cb7266c67b2c411660d572e937143d3db45cc080
Abhijeet Muneshwar <openingknots@gmail.com>
2024-08-04 15:25:06 +0530

    First Commit

---
+++ src/main/java/org/java8/Main.java
@@ -0,0 +1,13 @@
+package org.java8;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    04/08/24,
+ * Time:    3:15 pm
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
